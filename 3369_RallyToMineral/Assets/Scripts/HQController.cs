2c4352de0b6ed62fd6b7a050d42cdf8e9921ab4b
flightlessgames <gitcommit@flightlessgames.net>
2019-11-14 14:57:36 -0600

    Revert "Midnight Changes"
    This reverts commit bbe582fbb3aac8cd303e23d50e1a7002de060edc.

--- 3369_RallyToMineral/Assets/Scripts/HQController.cs
+++ 3369_RallyToMineral/Assets/Scripts/HQController.cs
@@ -7,21 +7,10 @@ public class HQController : ClickableUnit
 {
     public event Action<int> OnMineralGain = delegate { };
 
-    public static ResourceData Minerals = null;
+    private int _minerals = 50;
+    private int _gas = 0;
 
-    [Header("Required")]
     [SerializeField] MineralGroup _mineralGroup = null;
-
-    [Header("Settings")]
-    [SerializeField] ResourceData _mineralsData = null;
-
-    private void Awake()
-    {
-        if(Minerals == null)
-        {
-            Minerals = _mineralsData;
-        }
-    }
 
 
     protected override void InteractWithObject(GameObject target)
@@ -37,9 +26,8 @@ public class HQController : ClickableUnit
         {
             Debug.Log("Miner with Resources");
             //switch(miner.resourceEnum //case minerals
-            Minerals.currentResource += miner.HoldingResources;
+            _minerals += miner.HoldingResources;
             OnMineralGain?.Invoke(miner.HoldingResources);
-            Minerals.CallUpdate();
 
             miner.DepositResources(_mineralGroup);
         }
