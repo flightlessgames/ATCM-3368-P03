49983967a26686ba4a6b083360f9fec32995dc8a
flightlessgames <gitcommit@flightlessgames.net>
2019-11-14 14:57:29 -0600

    Revert "Oops"
    This reverts commit 02648221c130945daea9ededae3f20702319c2d1.

---
+++ 3369_RallyToMineral/Assets/Scripts/Unit_Motor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System;
+
+[RequireComponent(typeof(ClickableUnit))]
+[RequireComponent(typeof(NavMeshAgent))]
+public class Unit_Motor : MonoBehaviour
+{
+    public event Action OnMove = delegate { };
+
+    private ClickableUnit _controller;
+    private NavMeshAgent _agent;
+    
+    private void Awake()
+    {
+        _controller = GetComponent<ClickableUnit>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnEnable()
+    {
+        _controller.NewLocation += DriveToLocation;
+    }
+
+    public void DriveToLocation(Vector3 position)
+    {
+        _agent.destination = position;
+        OnMove?.Invoke();
+        Debug.DrawRay(position, Vector3.up * 5f, Color.red, 1f);
+
+    }
+}
